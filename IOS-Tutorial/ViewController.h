c710090c4ed2b5a511eac0c1d19fd472e395ac36
Ivan Skoryk <ivan.skoryk@icloud.com>
2018-07-23 12:37:34 +0300

    Initial Commit

---
+++ IOS-Tutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IOS-Tutorial
+//
+//  Created by Ivan Skoryk on 7/23/18.
+//  Copyright © 2018 FaiFly. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
